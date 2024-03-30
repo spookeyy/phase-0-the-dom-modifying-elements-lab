ec5e4ab571e5eddac1eb1e4b42096c358a30e538
spookeyy <pangasmeshack@gmail.com>
2024-03-31 00:06:18 +0300

    Dom manipulation

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+//Can be loaded in the html file
+
+const testVar = "hi"
+
+
+document.querySelector("main#main").remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "Meshack Pangas is the champion!";
+newHeader.className = "victory";
